6faec2f8be97d0b94487df333cc491287b02c94b
quachvankhanh <khanhqvj@gmail.com>
2025-01-16 15:37:15 +0700

    first commit 1

--- service/middlewares/authMiddleware.js
+++ service/middlewares/authMiddleware.js
@@ -1,17 +1,16 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
-// Middleware authenticateToken để xác thực token JWT
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.sendStatus(401); // Nếu không có token, trả về 401 Unauthorized
+  if (!token) return res.status(401).json({ error: "No token provided" });
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403); // Nếu xác thực token thất bại, trả về 403 Forbidden
+    if (err) return res.status(403).json({ error: "Invalid token" });
     req.user = user;
-    next(); // Nếu thành công, tiếp tục request
+    next();
   });
 }
 
